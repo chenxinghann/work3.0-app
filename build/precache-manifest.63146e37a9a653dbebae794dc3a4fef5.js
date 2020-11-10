b9284ec6a093b75194ff96caee31b8ca5ff9aa75
wyttttt <1301263327@qq.com>
2020-11-10 19:32:29 +0800

    Signed-off-by: chen

---
+++ build/precache-manifest.63146e37a9a653dbebae794dc3a4fef5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af93fe913b9dc2c37578ede40af95e14",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ff66f5cff934f861e223",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "db764e2e8091940db366",
+    "url": "./static/css/main.d5b2b956.chunk.css"
+  },
+  {
+    "revision": "ff66f5cff934f861e223",
+    "url": "./static/js/2.d7445c4c.chunk.js"
+  },
+  {
+    "revision": "9f81f95fdf77198dcf92d14b566e110e",
+    "url": "./static/js/2.d7445c4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db764e2e8091940db366",
+    "url": "./static/js/main.3e9d00a8.chunk.js"
+  },
+  {
+    "revision": "01b63068ce09b553f8be",
+    "url": "./static/js/runtime-main.05491e39.js"
+  }
+]);
